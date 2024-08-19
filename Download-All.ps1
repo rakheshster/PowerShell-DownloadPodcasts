ba6c4acadd6ebfb1d45c084efdd2016bebae10dc
Rakhesh Sasidharan <rakhesh@rakhesh.com>
2024-08-19 13:27:37 +0100

    bugfix

--- Download-All.ps1
+++ Download-All.ps1
@@ -1,6 +1,6 @@
 $myPath = $MyInvocation.MyCommand.Path
 $myFolder = Split-Path -Parent $myPath
 
-Get-ChildItem -Path $myFolder -Filter "Get-*.ps1" | ForEach-Object {
+Get-ChildItem -Path $myFolder -Filter "Get-*.ps1" | Where-Object { $_.Name -ne "Get-EmpirePodcast.ps1" } | ForEach-Object {
     Invoke-Expression $_
 }
